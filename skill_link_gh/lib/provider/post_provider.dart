8f3e4d3b9b3148449ac00d72ea23879787d0e178
Gee Suajay <surajmohammedbwoy@gmail.com>
2026-03-12 03:21:06 +0000

    Mar 12, 2026, 3:21 AM

--- skill_link_gh/lib/provider/post_provider.dart
+++ skill_link_gh/lib/provider/post_provider.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_riverpod/legacy.dart';
 import 'package:skill_link_gh/data/repository/post_repository.dart';
@@ -126,18 +127,38 @@ class PostsNotifier extends StateNotifier<PostsState> {
     if (index == -1) return;
 
     final post = state.posts[index];
-    await repository.toggleLike(postId);
+    final wasLiked = post.isLiked;
+    final newLikeCount = wasLiked ? post.likes - 1 : post.likes + 1;
 
+    // Update likedBy list
+    List<LikedByUser> newLikedBy = List.from(post.likedBy);
+
+    if (wasLiked) {
+      // Remove current user from likedBy when unliking
+      // If likes become 0, clear the entire list
+      if (newLikeCount == 0) {
+        newLikedBy = [];
+      }
+    } else {
+      // When liking, we'll fetch updated likedBy from server after the operation
+      // For now, just keep the existing list (it will be updated on next fetch)
+    }
+
+    // Optimistic update
     final updatedPosts = [
       ...state.posts.sublist(0, index),
       post.copyWith(
-        isLiked: !post.isLiked,
-        likes: post.isLiked ? post.likes - 1 : post.likes + 1,
+        isLiked: !wasLiked,
+        likes: newLikeCount,
+        likedBy: newLikedBy,
       ),
       ...state.posts.sublist(index + 1),
     ];
 
     state = state.copyWith(posts: updatedPosts);
+
+    // Perform actual like toggle
+    await repository.toggleLike(postId);
   }
 
   /// Toggle Save
