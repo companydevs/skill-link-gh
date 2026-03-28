0ce0b3b53eae5be7e82f72618c48482a8cc7a80d
Gee Suajay <surajmohammedbwoy@gmail.com>
2026-03-28 00:08:35 +0000

    Mar 28, 2026, 12:08 AM

---
+++ skill_link_gh/lib/presentation/service_booking_screen/widgets/location_input_widget.dart
@@ -0,0 +1,9 @@
+﻿import 'dart:developer' as dev;
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:sizer/sizer.dart';
+
+const _mapsApiKey = 'AIzaSyCeGxqoYlPBqAXDX5JMp89wwJfmQEM-ZWc';
