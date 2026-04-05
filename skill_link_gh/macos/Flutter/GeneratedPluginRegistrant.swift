36c1c28cd8d94e9ba1bc5706e0dc5a2faf8d702b
Gee Suajay <surajmohammedbwoy@gmail.com>
2026-04-05 00:14:29 +0000

    Apr 5, 2026, 12:14 AM

--- skill_link_gh/macos/Flutter/GeneratedPluginRegistrant.swift
+++ skill_link_gh/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -16,6 +16,7 @@ import firebase_core
 import firebase_storage
 import geolocator_apple
 import google_sign_in_ios
+import mobile_scanner
 import path_provider_foundation
 import share_plus
 import shared_preferences_foundation
@@ -37,6 +38,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
